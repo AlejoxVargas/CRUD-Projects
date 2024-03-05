7cb5f1b17d7e9d0e267c44dee02302a8ed46e663
AlejoxVargas <henrryalejandro21@gmail.com>
2024-03-05 10:37:02 +0100

    Add client-side routing and build scripts

--- MERN-MySQL-TodoList/server/index.js
+++ MERN-MySQL-TodoList/server/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import { PORT } from "./config.js";
+import path from "path";
 
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
+
+const__dirname = path.resolve();
 
 const app = express();
 
@@ -12,6 +15,10 @@ app.use(express.json());
 
 app.use(indexRoutes);
 app.use(taskRoutes);
+
+app.get("*" , (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html" ));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
